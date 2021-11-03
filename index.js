118a50aaf0982549029e55c380d938f389af3f86
Daniël <daniel@lngzl.nl>
2021-11-03 18:50:58 +0100

    adding commands

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
 const { Client, Intents } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGES,
+  ],
+});
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  client.api.applications(client.user.id).commands.post({
+    data: {
+      name: "ping",
+      description: "ping pong!",
+    },
+  });
 });
 
 client.on("interactionCreate", async (interaction) => {
